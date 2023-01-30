84bc17e9c3a2c5cb8513e6fd38a5ff9002da464e
Karamjit Singh <karamjitsingh1989@gmail.com>
2023-01-30 19:30:30 +0530

    Explaination about struct and class properties

---
+++ BasicsConcepts/ClassAndStatics.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+/*
+ 
+ Class vs statics
+ 
+ static variable are declared as stored property where as class variable declared as computed property.
+ 
+ 
+ 
+ */
+
+class Person {
+    
+  static  var name: String = "Karamjit Singh"
+    
+  class var address: String  {
+        return "Mohali"
+    }
+    
+    static func printMyAddress(){
+        
+    }
+    
+    class func printMyRole(){
+        
+    }
+}
+
+
+class Employee: Person {
+    
+    /*
+     Static function cannot be override and inherit
+     it show compliation error
+     
+     override static func printMyAddress(){
+            
+    }
+     Class method can be inerit
+     */
+    
+    override class func printMyRole() {
+        
+    }
+}
+
+
+struct Person1 {
+    
+    static var name: String = "Karamjit Singh"
+   
+   /* static property allowed in struct not class properties
+    class var address: String = "Chandigarh"
+    */
+}
